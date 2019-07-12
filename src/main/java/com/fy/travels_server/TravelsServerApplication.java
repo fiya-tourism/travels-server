0ff9245c460d2ba694880a4acf5ce13a3e7d65ba
e <17615201607@163.com>
2019-07-12 20:37:41 +0800

    shang

---
+++ src/main/java/com/fy/travels_server/TravelsServerApplication.java
@@ -0,0 +1,13 @@
+package com.fy.travels_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelsServerApplication.class, args);
+    }
+
+}
